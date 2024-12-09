e7881625964b1c0be84acc4c0ca7d8ea4ed0dadf
Pedro Perez <pedropm.lolo05@gmail.com>
2024-12-09 14:12:12 +0100

    no compila

--- module_basic.h
+++ module_basic.h
@@ -6,3 +6,4 @@ void Directorio(EXT_ENTRADA_DIR *directorio, EXT_BLQ_INODOS *inodos);
 
 // TODO: CHECK COMMAND
 int ComprobarComando(char *strcomando, char *orden, char *argumento1, char *argumento2);
+
